f630ce90293fbbda389ab34325fc1af6ef665ad9
Mujtaba Bader <ma2bader@uwaterloo.ca>
2024-01-24 09:40:22 -0500

    now runs gameengine instance

--- src/game/main.cpp
+++ src/game/main.cpp
@@ -1,7 +1,14 @@
-/*#include <iostream>
-
+#include "GameEngine.h"
+#include <stdexcept>
 
 int main() {
+	try {
+		GameEngine game;
+		game.run();
+	}
+	catch (std::exception& e) {
+		std::cout << e.what() << std::endl;
+	}
 
 	return 0;
-}*/
+}
