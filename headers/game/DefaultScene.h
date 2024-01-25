f63fc359b2b4b40846c415c73864673e5a9a2d4c
Mujtaba Bader <ma2bader@uwaterloo.ca>
2024-01-25 14:26:20 -0500

    added system for collision detection, lightsource entity, and several box entities

--- headers/game/DefaultScene.h
+++ headers/game/DefaultScene.h
@@ -8,14 +8,20 @@ class DefaultScene : public Scene {
 private:
 	EntityManager m_EM;
 	std::shared_ptr<Entity> m_Player;
+	std::shared_ptr<Entity> m_LightSource;
 
 public:
 	DefaultScene(GameEngine* gameEngine);
 
 	virtual void update(float);
+	void spawnPlayer();
+	void spawnBoxes(unsigned int);
+	void spawnLightSource();
+
 	virtual void sDoCommand(const Command&);
 	virtual void sRender();
 	void sMovement(float);
+	void sCollision();
 
 private:
 	void init();
--- src/game/DefaultScene.cpp
+++ src/game/DefaultScene.cpp
@@ -4,6 +4,7 @@
 #include "GameEngine.h"
 #include "ShaderTags.h"
 #include "VBO.h"
+#include "Physics.h"
 
 DefaultScene::DefaultScene(GameEngine* engine):
 	Scene(engine){
@@ -14,9 +15,87 @@ DefaultScene::DefaultScene(GameEngine* engine):
 void DefaultScene::init() {
 	registerCommand(GLFW_KEY_LEFT, CommandTags::Left);
 	registerCommand(GLFW_KEY_RIGHT, CommandTags::Right);
-	registerCommand(GLFW_KEY_UP, CommandTags::Forward);
-	registerCommand(GLFW_KEY_DOWN, CommandTags::Backward);
+	registerCommand(GLFW_KEY_UP, CommandTags::Up);
+	registerCommand(GLFW_KEY_DOWN, CommandTags::Down);
 
+	spawnLightSource();
+	spawnPlayer();
+	spawnBoxes(4);
+}
+
+void DefaultScene::spawnLightSource() {
+	float vertices[] = {
+		// positions // normals // texture coords
+		-0.5f, -0.5f, -0.5f, 0.0f, 0.0f, -1.0f, 0.0f, 0.0f,
+		0.5f, -0.5f, -0.5f, 0.0f, 0.0f, -1.0f, 1.0f, 0.0f,
+		0.5f, 0.5f, -0.5f, 0.0f, 0.0f, -1.0f, 1.0f, 1.0f,
+		0.5f, 0.5f, -0.5f, 0.0f, 0.0f, -1.0f, 1.0f, 1.0f,
+		-0.5f, 0.5f, -0.5f, 0.0f, 0.0f, -1.0f, 0.0f, 1.0f,
+		-0.5f, -0.5f, -0.5f, 0.0f, 0.0f, -1.0f, 0.0f, 0.0f,
+		-0.5f, -0.5f, 0.5f, 0.0f, 0.0f, 1.0f, 0.0f, 0.0f,
+		0.5f, -0.5f, 0.5f, 0.0f, 0.0f, 1.0f, 1.0f, 0.0f,
+		0.5f, 0.5f, 0.5f, 0.0f, 0.0f, 1.0f, 1.0f, 1.0f,
+		0.5f, 0.5f, 0.5f, 0.0f, 0.0f, 1.0f, 1.0f, 1.0f,
+		-0.5f, 0.5f, 0.5f, 0.0f, 0.0f, 1.0f, 0.0f, 1.0f,
+		-0.5f, -0.5f, 0.5f, 0.0f, 0.0f, 1.0f, 0.0f, 0.0f,
+		-0.5f, 0.5f, 0.5f, -1.0f, 0.0f, 0.0f, 1.0f, 0.0f,
+		-0.5f, 0.5f, -0.5f, -1.0f, 0.0f, 0.0f, 1.0f, 1.0f,
+		-0.5f, -0.5f, -0.5f, -1.0f, 0.0f, 0.0f, 0.0f, 1.0f,
+		-0.5f, -0.5f, -0.5f, -1.0f, 0.0f, 0.0f, 0.0f, 1.0f,
+		-0.5f, -0.5f, 0.5f, -1.0f, 0.0f, 0.0f, 0.0f, 0.0f,
+		-0.5f, 0.5f, 0.5f, -1.0f, 0.0f, 0.0f, 1.0f, 0.0f,
+		0.5f, 0.5f, 0.5f, 1.0f, 0.0f, 0.0f, 1.0f, 0.0f,
+		0.5f, 0.5f, -0.5f, 1.0f, 0.0f, 0.0f, 1.0f, 1.0f,
+		0.5f, -0.5f, -0.5f, 1.0f, 0.0f, 0.0f, 0.0f, 1.0f,
+		0.5f, -0.5f, -0.5f, 1.0f, 0.0f, 0.0f, 0.0f, 1.0f,
+		0.5f, -0.5f, 0.5f, 1.0f, 0.0f, 0.0f, 0.0f, 0.0f,
+		0.5f, 0.5f, 0.5f, 1.0f, 0.0f, 0.0f, 1.0f, 0.0f,
+		-0.5f, -0.5f, -0.5f, 0.0f, -1.0f, 0.0f, 0.0f, 1.0f,
+		0.5f, -0.5f, -0.5f, 0.0f, -1.0f, 0.0f, 1.0f, 1.0f,
+		0.5f, -0.5f, 0.5f, 0.0f, -1.0f, 0.0f, 1.0f, 0.0f,
+		0.5f, -0.5f, 0.5f, 0.0f, -1.0f, 0.0f, 1.0f, 0.0f,
+		-0.5f, -0.5f, 0.5f, 0.0f, -1.0f, 0.0f, 0.0f, 0.0f,
+		-0.5f, -0.5f, -0.5f, 0.0f, -1.0f, 0.0f, 0.0f, 1.0f,
+		-0.5f, 0.5f, -0.5f, 0.0f, 1.0f, 0.0f, 0.0f, 1.0f,
+		0.5f, 0.5f, -0.5f, 0.0f, 1.0f, 0.0f, 1.0f, 1.0f,
+		0.5f, 0.5f, 0.5f, 0.0f, 1.0f, 0.0f, 1.0f, 0.0f,
+		0.5f, 0.5f, 0.5f, 0.0f, 1.0f, 0.0f, 1.0f, 0.0f,
+		-0.5f, 0.5f, 0.5f, 0.0f, 1.0f, 0.0f, 0.0f, 0.0f,
+		-0.5f, 0.5f, -0.5f, 0.0f, 1.0f, 0.0f, 0.0f, 1.0f
+	};
+
+	m_LightSource = m_EM.addEntity(Entities::LightSoruce);
+	m_LightSource->addComponent<CTransform>();
+	m_LightSource->getComponent<CTransform>().pos = glm::vec3(1.0, 4.0, 10);
+	m_LightSource->getComponent<CTransform>().scale = glm::vec3(0.2);
+
+	m_LightSource->addComponent<CShader>(ResourceManager::LoadShader("assets/shaders/lightingShader.vert",
+		"assets/shaders/lightingShader.frag"));
+
+	m_LightSource->addComponent<CColor>();
+	m_LightSource->getComponent<CColor>().color = glm::vec3(1.0, 1.0, 1.0);
+
+	m_LightSource->addComponent<CHandle>();
+	unsigned int& boxVBO = m_LightSource->getComponent<CHandle>().VBO;
+	unsigned int& boxVAO = m_LightSource->getComponent<CHandle>().VAO;
+
+	glGenVertexArrays(1, &boxVAO);
+	glBindVertexArray(boxVAO);
+
+	VBO::BuildVBO(GL_ARRAY_BUFFER, boxVBO, vertices, sizeof(vertices),
+		GL_STATIC_DRAW);
+	VBO::BindVBO(GL_ARRAY_BUFFER, boxVBO);
+
+	for (int i = 0; i < 2; i++) {
+		VBO::EnableVBO(i, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float),
+			(void*)(i * 3 * sizeof(float)));
+	}
+
+	VBO::UnBindVBO(GL_ARRAY_BUFFER);
+	glBindVertexArray(0);
+}
+
+void DefaultScene::spawnPlayer() {
 	float vertices[] = {
 		// positions // normals // texture coords
 		-0.5f, -0.5f, -0.5f, 0.0f, 0.0f, -1.0f, 0.0f, 0.0f,
@@ -60,6 +139,8 @@ void DefaultScene::init() {
 	m_Player = m_EM.addEntity(Entities::Player);
 	m_Player->addComponent<CTransform>();
 	m_Player->getComponent<CTransform>().pos = glm::vec3(0, 0, -3);
+	
+	m_Player->addComponent<CBoundingBox>();
 
 	m_Player->addComponent<CInput>();
 
@@ -79,24 +160,103 @@ void DefaultScene::init() {
 	VBO::BuildVBO(GL_ARRAY_BUFFER, boxVBO, vertices, sizeof(vertices),
 		GL_STATIC_DRAW);
 	VBO::BindVBO(GL_ARRAY_BUFFER, boxVBO);
-	
+
 	for (int i = 0; i < 2; i++) {
 		VBO::EnableVBO(i, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float),
 			(void*)(i * 3 * sizeof(float)));
 	}
-	/*glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)0);
-	glEnableVertexAttribArray(0);
-	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(3 * sizeof(float)));
-	glEnableVertexAttribArray(1);*/
-	
+
 	VBO::UnBindVBO(GL_ARRAY_BUFFER);
 	glBindVertexArray(0);
+}
+
+void DefaultScene::spawnBoxes(unsigned int boxCount) {
+	float vertices[] = {
+		// positions // normals // texture coords
+		-0.5f, -0.5f, -0.5f, 0.0f, 0.0f, -1.0f, 0.0f, 0.0f,
+		0.5f, -0.5f, -0.5f, 0.0f, 0.0f, -1.0f, 1.0f, 0.0f,
+		0.5f, 0.5f, -0.5f, 0.0f, 0.0f, -1.0f, 1.0f, 1.0f,
+		0.5f, 0.5f, -0.5f, 0.0f, 0.0f, -1.0f, 1.0f, 1.0f,
+		-0.5f, 0.5f, -0.5f, 0.0f, 0.0f, -1.0f, 0.0f, 1.0f,
+		-0.5f, -0.5f, -0.5f, 0.0f, 0.0f, -1.0f, 0.0f, 0.0f,
+		-0.5f, -0.5f, 0.5f, 0.0f, 0.0f, 1.0f, 0.0f, 0.0f,
+		0.5f, -0.5f, 0.5f, 0.0f, 0.0f, 1.0f, 1.0f, 0.0f,
+		0.5f, 0.5f, 0.5f, 0.0f, 0.0f, 1.0f, 1.0f, 1.0f,
+		0.5f, 0.5f, 0.5f, 0.0f, 0.0f, 1.0f, 1.0f, 1.0f,
+		-0.5f, 0.5f, 0.5f, 0.0f, 0.0f, 1.0f, 0.0f, 1.0f,
+		-0.5f, -0.5f, 0.5f, 0.0f, 0.0f, 1.0f, 0.0f, 0.0f,
+		-0.5f, 0.5f, 0.5f, -1.0f, 0.0f, 0.0f, 1.0f, 0.0f,
+		-0.5f, 0.5f, -0.5f, -1.0f, 0.0f, 0.0f, 1.0f, 1.0f,
+		-0.5f, -0.5f, -0.5f, -1.0f, 0.0f, 0.0f, 0.0f, 1.0f,
+		-0.5f, -0.5f, -0.5f, -1.0f, 0.0f, 0.0f, 0.0f, 1.0f,
+		-0.5f, -0.5f, 0.5f, -1.0f, 0.0f, 0.0f, 0.0f, 0.0f,
+		-0.5f, 0.5f, 0.5f, -1.0f, 0.0f, 0.0f, 1.0f, 0.0f,
+		0.5f, 0.5f, 0.5f, 1.0f, 0.0f, 0.0f, 1.0f, 0.0f,
+		0.5f, 0.5f, -0.5f, 1.0f, 0.0f, 0.0f, 1.0f, 1.0f,
+		0.5f, -0.5f, -0.5f, 1.0f, 0.0f, 0.0f, 0.0f, 1.0f,
+		0.5f, -0.5f, -0.5f, 1.0f, 0.0f, 0.0f, 0.0f, 1.0f,
+		0.5f, -0.5f, 0.5f, 1.0f, 0.0f, 0.0f, 0.0f, 0.0f,
+		0.5f, 0.5f, 0.5f, 1.0f, 0.0f, 0.0f, 1.0f, 0.0f,
+		-0.5f, -0.5f, -0.5f, 0.0f, -1.0f, 0.0f, 0.0f, 1.0f,
+		0.5f, -0.5f, -0.5f, 0.0f, -1.0f, 0.0f, 1.0f, 1.0f,
+		0.5f, -0.5f, 0.5f, 0.0f, -1.0f, 0.0f, 1.0f, 0.0f,
+		0.5f, -0.5f, 0.5f, 0.0f, -1.0f, 0.0f, 1.0f, 0.0f,
+		-0.5f, -0.5f, 0.5f, 0.0f, -1.0f, 0.0f, 0.0f, 0.0f,
+		-0.5f, -0.5f, -0.5f, 0.0f, -1.0f, 0.0f, 0.0f, 1.0f,
+		-0.5f, 0.5f, -0.5f, 0.0f, 1.0f, 0.0f, 0.0f, 1.0f,
+		0.5f, 0.5f, -0.5f, 0.0f, 1.0f, 0.0f, 1.0f, 1.0f,
+		0.5f, 0.5f, 0.5f, 0.0f, 1.0f, 0.0f, 1.0f, 0.0f,
+		0.5f, 0.5f, 0.5f, 0.0f, 1.0f, 0.0f, 1.0f, 0.0f,
+		-0.5f, 0.5f, 0.5f, 0.0f, 1.0f, 0.0f, 0.0f, 0.0f,
+		-0.5f, 0.5f, -0.5f, 0.0f, 1.0f, 0.0f, 0.0f, 1.0f
+	};
+	glm::vec3 boxPos[] = {
+		glm::vec3(0,2,-3),
+		glm::vec3(0,-2,-3),
+		glm::vec3(2,0,-3),
+		glm::vec3(-2,0,-3),
+	};
+
+	for (int i = 0; i < boxCount; i++) {
+		auto& box = m_EM.addEntity(Entities::Box);
+		box = m_EM.addEntity(Entities::Player);
+		box->addComponent<CTransform>();
+		box->getComponent<CTransform>().pos = boxPos[i % 4];
+
+		box->addComponent<CBoundingBox>();
+
+		box->addComponent<CShader>(ResourceManager::LoadShader("assets/shaders/vertShader.vert",
+			"assets/shaders/fragShader.frag"));
+
+		box->addComponent<CColor>();
+		box->getComponent<CColor>().color = glm::vec3(0, 0, 1.0);
+
+		box->addComponent<CHandle>();
+		unsigned int& boxVBO = box->getComponent<CHandle>().VBO;
+		unsigned int& boxVAO = box->getComponent<CHandle>().VAO;
+
+		glGenVertexArrays(1, &boxVAO);
+		glBindVertexArray(boxVAO);
+
+		VBO::BuildVBO(GL_ARRAY_BUFFER, boxVBO, vertices, sizeof(vertices),
+			GL_STATIC_DRAW);
+		VBO::BindVBO(GL_ARRAY_BUFFER, boxVBO);
+
+		for (int i = 0; i < 2; i++) {
+			VBO::EnableVBO(i, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float),
+				(void*)(i * 3 * sizeof(float)));
+		}
+
+		VBO::UnBindVBO(GL_ARRAY_BUFFER);
+		glBindVertexArray(0);
+	}
 }
 
 void DefaultScene::update(float dt) {
 	m_EM.update();
 
 	sMovement(dt);
+	sCollision();
 	sRender();
 }
 
@@ -112,10 +272,10 @@ void DefaultScene::sMovement(float dt) {
 		playerVel.x = 5;
 	}
 	if (input.forward == true) {
-		playerVel.z = -5;
+		playerVel.y = 5;
 	}
 	if (input.backward == true) {
-		playerVel.z = 5;
+		playerVel.y = -5;
 	}
 	
 	transform.vel = playerVel;
@@ -160,6 +320,18 @@ void DefaultScene::sDoCommand(const Command& cmd){
 	}
 }
 
+void DefaultScene::sCollision() {
+	for (auto& e : m_EM.getEntities()) {
+		if (e->tag() == Entities::Player) {
+			continue;
+		}
+		glm::vec3 overlap = Physics::GetOverlap(m_Player, e);
+		if (overlap.x > 0 && overlap.y > 0 && overlap.z > 0) {
+			std::cout << "Collision Detected!" << std::endl;
+		}
+	}
+}
+
 void DefaultScene::sRender() {
 	glm::mat4 model = glm::mat4(1.0);
 	glm::mat4 view = m_Engine->getCamera().GetViewMatrix();
@@ -167,22 +339,59 @@ void DefaultScene::sRender() {
 		1280.f / 720.f, 0.1f, 100.f);
 
 	for (auto& e : m_EM.getEntities()) {
-		if (e->tag() == Entities::Player) {
+		if (e->tag() != Entities::LightSoruce) {
+			if (e->hasComponent<CTransform>() &&
+				e->hasComponent<CShader>() &&
+				e->hasComponent<CColor>() &&
+				e->hasComponent<CHandle>() &&
+				e->hasComponent<CBoundingBox>()) {
+				model = glm::scale(model, e->getComponent<CTransform>().scale);
+				model = glm::translate(model, e->getComponent<CTransform>().pos);
+				e->getComponent<CShader>().shader.use();
+				e->getComponent<CShader>().shader.setMat4("model", model);
+				e->getComponent<CShader>().shader.setMat4("view", view);
+				e->getComponent<CShader>().shader.setMat4("projection", proj);
+
+				e->getComponent<CShader>().shader.setVec3("material.ambient",
+					e->getComponent<CColor>().color);
+				e->getComponent<CShader>().shader.setVec3("material.diffuse",
+					e->getComponent<CColor>().color);
+				e->getComponent<CShader>().shader.setVec3("material.specular",
+					glm::vec3(1.0, 1.0, 0.0));
+				e->getComponent<CShader>().shader.setFloat("material.shininess",
+					32.f);
+
+				e->getComponent<CShader>().shader.setVec3("light.ambient",
+					glm::vec3(0.2));
+				e->getComponent<CShader>().shader.setVec3("light.diffuse",
+					glm::vec3(0.5));
+				e->getComponent<CShader>().shader.setVec3("light.specular",
+					glm::vec3(1.0));
+
+				e->getComponent<CShader>().shader.setVec3("lightPos",
+					m_LightSource->getComponent<CTransform>().pos);
+
+				glBindVertexArray(e->getComponent<CHandle>().VAO);
+				glDrawArrays(GL_TRIANGLES, 0, 36);
+
+				model = glm::mat4(1.0);
+			}
+		}
+		else {
 			model = glm::scale(model, e->getComponent<CTransform>().scale);
 			model = glm::translate(model, e->getComponent<CTransform>().pos);
 			e->getComponent<CShader>().shader.use();
 			e->getComponent<CShader>().shader.setMat4("model", model);
 			e->getComponent<CShader>().shader.setMat4("view", view);
 			e->getComponent<CShader>().shader.setMat4("projection", proj);
-			e->getComponent<CShader>().shader.setVec3("objColor",
-				e->getComponent<CColor>().color);
+
 			e->getComponent<CShader>().shader.setVec3("lightColor",
-				glm::vec3(1.0, 1.0, 1.0));
-			e->getComponent<CShader>().shader.setVec3("lightPos",
-				glm::vec3(3.2, 5.0, 3.0));
+				e->getComponent<CColor>().color);
 
 			glBindVertexArray(e->getComponent<CHandle>().VAO);
 			glDrawArrays(GL_TRIANGLES, 0, 36);
+
+			model = glm::mat4(1.0);
 		}
 	}
 }
