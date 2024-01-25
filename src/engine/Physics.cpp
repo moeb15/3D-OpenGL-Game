7e748a4b4cc826f0d15fa41f06c33124cfcaa33c
Mujtaba Bader <ma2bader@uwaterloo.ca>
2024-01-25 14:49:53 -0500

    recalcualated dx, dy, dz, had to append half the size in a given spatial diemnsion to center origin of entity

--- src/engine/Physics.cpp
+++ src/engine/Physics.cpp
@@ -10,9 +10,9 @@ glm::vec3 Physics::GetOverlap(std::shared_ptr<Entity>& e1, std::shared_ptr<Entit
 		glm::vec3& e1Pos = e1->getComponent<CTransform>().pos;
 		glm::vec3& e2Pos = e2->getComponent<CTransform>().pos;
 
-		float dx = fabsf(e1Pos.x - e2Pos.x);
-		float dy = fabsf(e1Pos.y - e2Pos.y);
-		float dz = fabsf(e1Pos.z - e2Pos.z);
+		float dx = fabsf(e1Pos.x - e2Pos.x + e1Size.x / 2.f - e2Size.x / 2.f);
+		float dy = fabsf(e1Pos.y - e2Pos.y + e1Size.y / 2.f - e2Size.y / 2.f);
+		float dz = fabsf(e1Pos.z - e2Pos.z + e1Size.z / 2.f - e2Size.z / 2.f);
 
 		return glm::vec3(
 			(e1Size.x + e2Size.x) / 2.f - dx,
@@ -33,9 +33,9 @@ glm::vec3 Physics::GetPreviousOverlap(std::shared_ptr<Entity>& e1, std::shared_p
 		glm::vec3& e1Pos = e1->getComponent<CTransform>().prevPos;
 		glm::vec3& e2Pos = e2->getComponent<CTransform>().prevPos;
 
-		float dx = fabsf(e1Pos.x - e2Pos.x);
-		float dy = fabsf(e1Pos.y - e2Pos.y);
-		float dz = fabsf(e1Pos.z - e2Pos.z);
+		float dx = fabsf(e1Pos.x - e2Pos.x + e1Size.x / 2.f - e2Size.x / 2.f);
+		float dy = fabsf(e1Pos.y - e2Pos.y + e1Size.y / 2.f - e2Size.y / 2.f);
+		float dz = fabsf(e1Pos.z - e2Pos.z + e1Size.z / 2.f - e2Size.z / 2.f);
 
 		return glm::vec3(
 			(e1Size.x + e2Size.x) / 2.f - dx,
