17bd3d63783aed6d5e2c12f26106b962a64f5abd
Mujtaba Bader <ma2bader@uwaterloo.ca>
2024-01-27 06:10:02 -0500

    added LoadTexture static method

--- headers/engine/ResourceManager.h
+++ headers/engine/ResourceManager.h
@@ -2,6 +2,7 @@
 #define RESOURCEMANAGER_H
 
 #include "Shader.h"
+#include "Texture.h"
 
 class ResourceManager {
 public:
@@ -9,6 +10,9 @@ public:
 
 	// Loads a shader given the vertex and fragment shader file paths
 	static const Shader& LoadShader(const char* vert, const char* frag);
+
+	// Loads a texture given an image file path
+	static const Texture& LoadTexture(const char* path);
 };
 
 #endif 
--- src/engine/ResourceManager.cpp
+++ src/engine/ResourceManager.cpp
@@ -4,3 +4,7 @@
 const Shader& ResourceManager::LoadShader(const char* vert, const char* frag) {
 	return Shader(vert, frag);
 }
+
+const Texture& ResourceManager::LoadTexture(const char* path) {
+	return Texture(path);
+}
