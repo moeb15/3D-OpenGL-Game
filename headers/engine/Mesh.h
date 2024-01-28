d425c895a9c7e80c8972f4ddff816c60b9ac8727
Mujtaba Bader <ma2bader@uwaterloo.ca>
2024-01-28 09:38:51 -0500

    class for loading models and model meshes

---
+++ headers/engine/Mesh.h
@@ -0,0 +1,42 @@
+#ifndef MESH_H
+#define MESH_H
+
+#include "glad.h"
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <vector>
+#include <string>
+#include "Shader.h"
+
+struct Vertex {
+	glm::vec3 Position;
+	glm::vec3 Normal;
+	glm::vec2 TexCoords;
+};
+
+struct Texture {
+	unsigned int id;
+	std::string type;
+	std::string path;
+};
+
+class Mesh {
+public:
+	// mesh data
+	std::vector<Vertex> verticies;
+	std::vector<unsigned int> indicies;
+	std::vector<Texture> textures;
+
+	Mesh(std::vector<Vertex>, std::vector<unsigned int>,
+		std::vector<Texture>);
+	void Draw(Shader& shader);
+	
+private:
+	// render data
+	unsigned int VAO, VBO, EBO;
+
+	void setupMesh();
+
+};
+
+#endif // !MESH_H
---
+++ headers/engine/Model.h
@@ -0,0 +1,31 @@
+#ifndef MDOEL_H
+#define MODEL_H
+
+#include "Mesh.h"
+#include <assimp/mesh.h>
+#include <assimp/scene.h>
+#include <assimp/Importer.hpp>
+
+class Model {
+public:
+	std::vector<Texture> textures_loaded;
+
+	Model(const char* path) {
+		loadModel(path);
+	}
+
+	void Draw(Shader& shader);
+
+private:
+	// model data
+	std::vector<Mesh> meshes;
+	std::string directory;
+
+	void loadModel(std::string path);
+	void processNode(aiNode* node, const aiScene* scene);
+	Mesh processMesh(aiMesh* mesh, const aiScene* scene);
+	std::vector<Texture> loadMaterialTextures(aiMaterial* mat,
+		aiTextureType type, std::string typeName);
+};
+
+#endif // !MDOEL_H
