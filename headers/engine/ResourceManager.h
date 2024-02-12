33f7ef140eea80a3f9b60380e74c045bcd9a4848
Mujtaba Bader <ma2bader@uwaterloo.ca>
2024-02-11 19:52:58 -0500

    no longer a static class, now contains methods for adding resoruces, stores unique pointers to resources in hashmaps so resources are only loaded once and not each time the corresponding load method is called

--- headers/engine/ResourceManager.h
+++ headers/engine/ResourceManager.h
@@ -3,23 +3,39 @@
 
 #include "Shader.h"
 #include "Texture.h"
+#include "ResourceTags.h"
+#include <unordered_map>
+#include <memory>
 
 namespace Models {
 	class Model;
 }
 
 class ResourceManager {
+private:
+	std::unordered_map<ResourceTags::Textures, std::unique_ptr<Texture>> m_Textures;
+	std::unordered_map<ResourceTags::Shaders, std::unique_ptr<Shader>> m_Shaders;
+	std::unordered_map<ResourceTags::Models, std::unique_ptr<Models::Model>> m_Models;
+
 public:
-	ResourceManager() = delete;
+	// Adds a shader given the shader tag, and the vertex and fragment shader file paths
+	void AddShader(ResourceTags::Shaders shaderTag, const char* vert, const char* frag);
 
-	// Loads a shader given the vertex and fragment shader file paths
-	static const Shader& LoadShader(const char* vert, const char* frag);
+	// Adds a texture given the texture tag and an image file path
+	void AddTexture(ResourceTags::Textures textureTag, const char* path);
 
-	// Loads a texture given an image file path
-	static const Texture& LoadTexture(const char* path);
+	// Adds a model fiven the model tag and the model file path
+	void AddModel(ResourceTags::Models modelTag, const char* path);
 
-	// Loads a model given the model file path
-	static Models::Model LoadModel(const char* path);
+	// Loads a shader given the shader tag
+	const Shader& LoadShader(ResourceTags::Shaders shaderTag);
+
+	// Loads a texture given a texture tag
+	const Texture& LoadTexture(ResourceTags::Textures textureTag);
+
+	// Loads a model given the model tag
+	const Models::Model& LoadModel(ResourceTags::Models modelTag);
+
 };
 
 #endif 
--- src/engine/ResourceManager.cpp
+++ src/engine/ResourceManager.cpp
@@ -1,14 +1,51 @@
 #include "ResourceManager.h"
 #include "Model.h"
 
-const Shader& ResourceManager::LoadShader(const char* vert, const char* frag) {
-	return Shader(vert, frag);
+
+void ResourceManager::AddShader(ResourceTags::Shaders shaderTag, const char* vert, const char* frag) {
+	std::unique_ptr<Shader> shader(
+		new Shader(vert, frag));
+
+	m_Shaders.insert(std::make_pair(shaderTag, std::move(shader)));
 }
 
-const Texture& ResourceManager::LoadTexture(const char* path) {
-	return Texture(path);
+void ResourceManager::AddTexture(ResourceTags::Textures textureTag, const char* path) {
+	std::unique_ptr<Texture> texture(
+		new Texture(path));
+
+	m_Textures.insert(std::make_pair(textureTag, std::move(texture)));
 }
 
-Models::Model ResourceManager::LoadModel(const char* path) {
-	return Models::Model(path);
+void ResourceManager::AddModel(ResourceTags::Models modelTag, const char* path) {
+	std::unique_ptr<Models::Model> model(
+		new Models::Model(path));
+
+	m_Models.insert(std::make_pair(modelTag, std::move(model)));
+}
+
+const Shader& ResourceManager::LoadShader(ResourceTags::Shaders shaderTag) {
+	auto kvPair = m_Shaders.find(shaderTag);
+	if(kvPair == m_Shaders.end()){
+		throw std::runtime_error("ResourceManager::LoadShader failed to load shader: " 
+			+ std::to_string(shaderTag));
+	}
+	return *kvPair->second;
+}
+
+const Texture& ResourceManager::LoadTexture(ResourceTags::Textures textureTag) {
+	auto kvPair = m_Textures.find(textureTag);
+	if (kvPair == m_Textures.end(textureTag)) {
+		throw std::runtime_error("ResourceManager::LoadTexture failed to load texture: "
+			+ std::to_string(textureTag));
+	}
+	return *kvPair->second;
+}
+
+const Models::Model& ResourceManager::LoadModel(ResourceTags::Models modelTag) {
+	auto kvPair = m_Models.find(modelTag);
+	if (kvPair == m_Models.end()) {
+		throw std::runtime_error("ResourceManager::LoadModel failed to load mode: "
+			+ std::to_string(modelTag));
+	}
+	return *kvPair->second;
 }
