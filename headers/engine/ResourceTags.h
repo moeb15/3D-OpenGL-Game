e9c8982511b4567a565d5087b1301c4b4fff062b
Mujtaba Bader <ma2bader@uwaterloo.ca>
2024-02-11 19:51:11 -0500

    contains tags for resources

---
+++ headers/engine/ResourceTags.h
@@ -0,0 +1,26 @@
+#ifndef RESOURCE_TAGS_H
+#define RESOURCE_TAGS_H
+
+namespace ResourceTags {
+	enum Textures {
+		PlayerDiffuse,
+		PlayerSpecular,
+		PlayerEmission,
+		ContainerDiffuse,
+		ContainerSpecular,
+		ContainerEmission,
+	};
+
+	enum Shaders {
+		DefaultShader,
+		LightingShader,
+		ModelShader,
+	};
+
+	enum Models {
+		DefaultModel,
+	};
+}
+
+#endif // !RESOURCE_TAGS_H
+
